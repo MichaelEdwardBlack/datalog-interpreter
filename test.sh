32b14459f3bee4c6d1ddb03e037cca4e858b1d07
michaelblack117 <michaelblack117@gmail.com>
2018-11-10 13:58:46 -0700

    adding test cases

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 INPUT_PATH="test/inputs"
 OUTPUT_PATH="test/outputs"
 EXPECTED_PATH="test/expected_outputs"
-PROGRAM="lab3"
+PROGRAM="lab4"
 
 if [ $# = 0 ]; then
 	g++ -g -Wall -std=c++17 -o ${PROGRAM} *.h *.cpp
-	for i in 1 2 3 4 5 6 7 8 9 10 11 12
+	for i in 1 2 3 4 5 6 7 8 9 10 11
 	do
 		./${PROGRAM} "${INPUT_PATH}/in${i}.txt" > "${OUTPUT_PATH}/out${i}.txt"
 		DIFF=$(diff ${OUTPUT_PATH}/out${i}.txt ${EXPECTED_PATH}/expected_out${i}.txt)
@@ -15,12 +15,13 @@ if [ $# = 0 ]; then
 			echo "Test for in${i}.txt passed!"
 	   	else
 			echo "Test for in${i}.txt failed!"
-			tkdiff ${OUTPUT_PATH}/out${i}.txt ${EXPECTED_PATH}/expected_out${i}.txt
+			diff ${OUTPUT_PATH}/out${i}.txt ${EXPECTED_PATH}/expected_out${i}.txt
     		fi
 	done
 fi
 while [ "$1" != "" ]; do
 	case $1 in
+		### run a specific test number ###
 		-t | --test)
 			shift
 			./${PROGRAM} "${INPUT_PATH}/in$1.txt" > "${OUTPUT_PATH}/out$1.txt"
@@ -33,24 +34,35 @@ while [ "$1" != "" ]; do
 				tkdiff ${OUTPUT_PATH}/out$1.txt ${EXPECTED_PATH}/expected_out$1.txt
 			fi
 			;;
-		-f | --file)
+		### run the program with a specified input file ###
+		-i | --input)
 			shift
 			./${PROGRAM} $1
 			;;
+		### run the program with no input files ###
 		-r | --run)
 			./${PROGRAM}
 			;;
+		### run the complexity test ###
 		-c | --complexity)
 			pmccabe *.cpp
 			;;
+		### display the differences between your output and the expected output ###
+		### requires a test number immediately following the flag ###
+		-d | --display)
+			shift
+			tkdiff ${OUTPUT_PATH}/out$1.txt ${EXPECTED_PATH}/expected_out$1.txt
+			;;
+		### display the help menu ###
 		-h | --help)
 			echo "Usage: $0 [option...] {test_number|file_name}" >&2
     			echo
 			echo "			no flags will run all test cases"
 			echo "	-t, --test	run test on test file specified by the file number"
-			echo "	-f, --file	run test on test file specified by the file name"
+			echo "	-i, --input	run test on input file specified by the file name"
       			echo "	-r, --run	run program with no inputs files"
 			echo " 	-c, --complexity	run complexity test (complexity > 8 is too high)"
+			echo "	-d,	--display	displays the differences of output and expected output of specified test number"
 			echo "	-h, --help	display the help"
 	   		echo
 
@@ -61,6 +73,7 @@ while [ "$1" != "" ]; do
 			echo "	./test.sh -f input20.txt"
 			echo "	./test.sh --run"
 			echo "	./test.sh -c -t 10"
+			echo "	./test.sh --display 7"
 			echo "	./test.sh --help"
 		    	exit 0
 			;;
